ff59ece148336dac38fa1cfb7ed3f36061133a02
rturner <rturner@basecampcodingacademy.org>
2018-07-26 10:10:39 -0500

    prints in receipt

--- shell.py
+++ shell.py
@@ -1,65 +1,80 @@
 import core
+import disk
 from datetime import datetime
 
 
+def in_stock(inventory, stock):
+    for item_name in inventory:
+        if inventory[item_name]['stock'] != 0:
+            return
+    else:
+        False
+
+
+def add_to_stock(inventory, stock):
+    new_inv += 1
+    return new_inv
+
+
 def main():
+    read = disk.open_file
     inventory = {
         'camaro': {
             'name': 'camaro',
-            'price': 180,
+            'price': 180 * .07,
             'stock': 4,
             'replacement': 150
         },
         '4-wheeler': {
             'name': '4-wheeler',
-            'price': 29,
+            'price': 29 * .07,
             'stock': 15,
             'replacement': 150,
         },
         'skeet jet': {
             'name': 'skeet jet',
-            'price': 43,
+            'price': 43 * .07,
             'stock': 8,
             'replacement': 37
         }
     }
 
-    with open('inventory.txt', 'r') as file:
-        file.readlines()
-        line = file.readlines
-    file = 'inventory.txt'
     # with open('history.txt', 'w') as file:
     #     file.write(text)
 
-    name = input("What's you name?")
+    name = input("What's you name?").strip()
     print('camaro, 4-wheeler, skeet jet')
-    user_choice = input('What would you like to rent today ' + name + '?')
     receipt = []
     while True:
-        if user_choice == 'camaro':
-            receipt.append(inventory['camaro'])
-            inventory['camaro']['stock'] -= 1
-            print(inventory['camaro'])
-            break
-        if user_choice == '4-wheeler':
-            receipt.append(inventory['4-wheeler'])
-            inventory['4-wheeler']['stock'] -= 1
-            print(inventory['4-wheeler'])
-            break
-        if user_choice == 'skeet jet':
-            receipt.append(inventory['skeet jet'])
-            print(inventory['skeet jet'])
-            inventory['camaro']['stock'] -= 1
-            break
-        else:
-            print('invalid request')
-            break
+        while True:
+            user_choice = input('What would you like to rent today ' + name +
+                                '?').strip().lower()
+            if user_choice == 'camaro':
+                receipt.append(inventory['camaro'])
+                inventory['camaro']['stock'] -= 1
+                print(inventory['camaro'])
+                break
+            elif user_choice == '4-wheeler':
+                receipt.append(inventory['4-wheeler'])
+                inventory['4-wheeler']['stock'] -= 1
+                print(inventory['4-wheeler'])
+                break
+            elif user_choice == 'skeet jet':
+                receipt.append(inventory['skeet jet'])
+                print(inventory['skeet jet'])
+                inventory['skeet jet']['stock'] -= 1
+                break
+            elif user_choice == 'done':
+                break
+            else:
+                print('invalid request')
+                break
 
     print(receipt)
 
     # def history(payment,  amount):
     #     time = datetime.now()
-    #     text = '/n{}, {}, {}' .format(time , payment, amount)
+    #     text = '/n{}, {}, {}' .format(time , item, amount)
 
     # with open('history.txt', 'w') as file:
     #         file.write(text)
