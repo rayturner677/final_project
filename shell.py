567128385ecab711eaa713a260c880077aed79b1
rturner <rturner@basecampcodingacademy.org>
2018-07-26 15:38:52 -0500

    cleaner code

--- shell.py
+++ shell.py
@@ -3,17 +3,21 @@ import disk
 from datetime import datetime
 
 
-def in_stock(inventory, stock):
-    for item_name in inventory:
-        if inventory[item_name]['stock'] != 0:
-            return
+def in_stock(inventory, item_name):
+    if item_name in inventory and inventory[item_name]['stock'] > 0:
+        return True
     else:
         False
 
 
-def add_to_stock(inventory, stock):
-    new_inv += 1
-    return new_inv
+def price_with_tax(inventory, item_name):
+    price = inventory[item_name]['price'] * 1.07
+    return round(price, 2)
+
+
+def add_to_stock(inventory, item_name):
+    inventory[item_name]['stock'] += 1
+    return
 
 
 def main():
@@ -21,19 +25,19 @@ def main():
     inventory = {
         'camaro': {
             'name': 'camaro',
-            'price': 180 * 1.07,
+            'price': 180,
             'stock': 4,
             'replacement': 150
         },
         '4-wheeler': {
             'name': '4-wheeler',
-            'price': 29 * 1.07,
+            'price': 29,
             'stock': 15,
             'replacement': 150,
         },
         'skeet jet': {
             'name': 'skeet jet',
-            'price': 43 * 1.07,
+            'price': 43,
             'stock': 8,
             'replacement': 37
         }
@@ -45,43 +49,21 @@ def main():
     name = input("What's you name?").strip()
     print('camaro, 4-wheeler, skeet jet')
     receipt = []
-
     while True:
-        if inventory['camaro']['stock'] == 0:
+        user_choice = input('What would you like to rent today ' + name +
+                            '?').strip().lower()
+        if in_stock(inventory, user_choice):
+            receipt.append(inventory[user_choice]['price'])
+            receipt append(inventory[user_choice]['replacement'])
+            inventory[user_choice]['stock'] -= 1
+            print(inventory[user_choice])
+            print('cost:', price_with_tax(inventory, user_choice))
+        elif user_choice == 'done':
             break
-        if inventory['4-wheeler']['stock'] == 0:
-            break
-        if inventory['skeet jet']['stock'] == 0:
-            break
-        while True:
-            user_choice = input('What would you like to rent today ' + name +
-                                '?').strip().lower()
-            if user_choice == 'camaro':
-                receipt.append(inventory['camaro'])
-                inventory['camaro']['stock'] -= 1
-                print(inventory['camaro'])
-                break
-            elif user_choice == '4-wheeler':
-                receipt.append(inventory['4-wheeler'])
-                inventory['4-wheeler']['stock'] -= 1
-                print(inventory['4-wheeler'])
-                break
-            elif user_choice == 'skeet jet':
-                receipt.append(inventory['skeet jet'])
-                print(inventory['skeet jet'])
-                inventory['skeet jet']['stock'] -= 1
-                break
-            elif user_choice == 'done':
-                break
-            else:
-                print('invalid request')
-                return
+        else:
+            print('Not in stock')
 
-    while True:
-        print('alive')
-        break
-
-    print(receipt)
+    print('total:' receipt)
 
     # def history(payment,  amount):
     #     time = datetime.now()
