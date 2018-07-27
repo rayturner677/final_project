13810e4047f0ac3c4376febe50de8f84b35d08ac
rturner <rturner@basecampcodingacademy.org>
2018-07-27 09:51:32 -0500

    sign complete

--- shell.py
+++ shell.py
@@ -20,6 +20,18 @@ def add_to_stock(inventory, item_name):
     return
 
 
+def find_replacement(inventory, item_name):
+    return
+
+
+def get_receipt(inventory, item_name):
+    receipt = []
+    receipt.append(item_name)
+    price = inventory[item_name]['price']
+    replacement = inventory[item_name]['replacement']
+    return '{} {} {}'.format('total:', price, replacement)
+
+
 def main():
     read = disk.open_file
     inventory = {
@@ -27,51 +39,61 @@ def main():
             'name': 'camaro',
             'price': 180,
             'stock': 4,
-            'replacement': 150
+            'replacement': 40000
         },
         '4-wheeler': {
             'name': '4-wheeler',
             'price': 29,
             'stock': 15,
-            'replacement': 150,
+            'replacement': 6000,
         },
-        'skeet jet': {
-            'name': 'skeet jet',
+        'jet ski': {
+            'name': 'jet ski',
             'price': 43,
             'stock': 8,
-            'replacement': 37
+            'replacement': 5000
         }
     }
-
-    # with open('history.txt', 'w') as file:
-    #     file.write(text)
-
+    sign_in = input('         Employee(1) or Costumer:(2)').strip().lower()
     name = input("What's you name?").strip()
-    print('camaro, 4-wheeler, skeet jet')
+    print('camaro, 4-wheeler, jet ski')
     receipt = []
     while True:
-        user_choice = input('What would you like to rent today ' + name +
-                            '?').strip().lower()
-        if in_stock(inventory, user_choice):
-            receipt.append(inventory[user_choice]['price'])
-            receipt append(inventory[user_choice]['replacement'])
-            inventory[user_choice]['stock'] -= 1
-            print(inventory[user_choice])
-            print('cost:', price_with_tax(inventory, user_choice))
-        elif user_choice == 'done':
-            break
-        else:
-            print('Not in stock')
+        if sign_in == '1':
+            user_choice = input('What would you like to rent today ' + name +
+                                '?').strip().lower()
+            if in_stock(inventory, user_choice):
+                get_receipt(inventory, user_choice)
+                inventory[user_choice]['stock'] -= 1
+                print(inventory[user_choice])
+                print('cost:', price_with_tax(inventory, user_choice))
+            elif user_choice == 'done':
+                break
+            else:
+                print('Not in stock')
 
-    print('total:' receipt)
+        if sign_in == '2':
+            user_choice = input('What would you like to rent today ' + name +
+                                '?').strip().lower()
+            if in_stock(inventory, user_choice):
+                get_receipt(inventory, user_choice)
+                inventory[user_choice]['stock'] -= 1
+                print(inventory[user_choice])
+                print('cost:', price_with_tax(inventory, user_choice))
+            elif user_choice == 'done':
+                break
+            else:
+                print('Not in stock')
 
-    # def history(payment,  amount):
-    #     time = datetime.now()
-    #     text = '/n{}, {}, {}' .format(time , item, amount)
+    print(receipt)
 
-    # with open('history.txt', 'w') as file:
-    #         file.write(text)
 
+# def history(payment,  amount):
+#     time = datetime.now()
+#     text = '/n{}, {}, {}' .format(time , item, amount)
+
+# with open('history.txt', 'w') as file:
+#         file.write(text)
 
 if __name__ == '__main__':
     main()
