972dc8a005913e718e03b93161d6e2371b758b40
rturner <rturner@basecampcodingacademy.org>
2018-07-31 09:37:16 -0500

    1.0 done

--- shell.py
+++ shell.py
@@ -80,19 +80,28 @@ def main():
                 cost_indays = inventory[user_choice]['price'] * user_days.isdigit(
                 )
                 print('cost with days rented:', cost_indays)
+                time = datetime.now()
+                taxed = price_with_tax(inventory, user_choice)
+                replacement = find_replacement(inventory, user_choice)
+                cost_indays = inventory[user_choice]['price'] * int(user_days)
+                total = taxed + cost_indays + replacement
+                text = '\n{}, {}, {}'.format(
+                    time, inventory[user_choice]['name'], total)
+                with open('history.txt', 'a') as file:
+                    data = file.write(text)
             elif user_choice == 'done':
                 break
             elif user_days == '':
                 break
             else:
                 print('Not in stock')
-        print('camaro, 4-wheeler, jet ski')
         if sign_in == '2':
+            print('camaro, 4-wheeler, jet ski')
             user_choice = input('What would you like to rent today ' + name +
                                 '?').strip().lower()
-            user_days = input(
-                'How long would you like to rent our merchandise?')
             if in_stock(inventory, user_choice):
+                user_days = input(
+                    'How long would you like to rent our merchandise?')
                 inventory[user_choice]['stock'] -= 1
                 taxed = price_with_tax(inventory, user_choice)
                 cost_indays = inventory[user_choice]['price'] * user_days.isdigit(
@@ -104,7 +113,6 @@ def main():
                     time, inventory[user_choice]['name'], total)
                 with open('history.txt', 'a') as file:
                     data = file.write(text)
-                    print(data)
             elif user_choice == 'done':
                 break
             else:
