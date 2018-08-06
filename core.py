2d213d5736c17f478082ba950bb2bc888f9d5e3b
rturner <rturner@basecampcodingacademy.org>
2018-08-06 15:35:06 -0500

    runs

---
+++ core.py
@@ -0,0 +1,30 @@
+def in_stock(inventory, item_name):
+    if item_name in inventory and inventory[item_name]['stock'] > 0:
+        return True
+    else:
+        False
+
+
+def price_with_tax(inventory, item_name):
+    price = inventory[item_name]['price'] * 1.07
+    return round(price, 2)
+
+
+def add_to_stock(inventory, return_choice):
+    inventory[return_choice]['stock'] += 1
+    return
+
+
+def find_replacement(inventory, item_name):
+    return int(inventory[item_name]['replacement']) * .10
+
+
+def get_total(inventory, item_name, items):
+    receipt = []
+    total = 0
+    for item in items:
+        receipt.append(item)
+    for item in items:
+        total += inventory[item_name]['price']
+    return receipt
+    print(receipt)
--- disk.py
+++ disk.py
@@ -1,9 +1,34 @@
-def open_file(filename):
-    with open(filename, 'r') as file:
-        file.readline()
-        file_information = file.readlines()
+from datetime import datetime
+import core
 
 
-def write_file(filename, file_string):
-    with open(filename, 'w'):
-        file.write(file_string)
+def read_file():
+    with open('inventory.txt', 'r') as file:
+        data = file.readlines()
+    inventory = {}
+    for item_info in data:
+        item = item_info.split(',')
+        item_name = item[0].strip()
+        price = int(item[1].strip())
+        stock = int(item[2].strip())
+        replacement = int(item[3].strip())
+        inventory[item_name] = {
+            'name': item_name,
+            'price': price,
+            'stock': stock,
+            'replacement': replacement
+        }
+    return inventory
+
+
+def write_file(inventory, user_choice, user_days):
+    taxed = core.price_with_tax(inventory, user_choice)
+    cost_indays = inventory[user_choice]['price'] * int(user_days)
+    replacement = core.find_replacement(inventory, user_choice)
+    total = round(taxed + cost_indays + replacement, 2)
+    time = datetime.now()
+
+    text = '\n{}, {}, {}'.format(time, inventory[user_choice]['name'], total)
+
+    with open('history.txt', 'a') as file:
+        data = file.write(text)
